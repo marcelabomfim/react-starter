718838c97eda3dc5421851832a36c6253b3b81d9
Marcela Bomfim <marcela.bomfim@lojaskd.com.br>
2018-06-04 13:09:07 -0300

    powered styled-components with polished sass style

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
+import { lighten } from 'polished';
 
 export default styled.h1`
   font-size: 1.5em;
   color: #1565c0;
   text-align: center;
+
+  &:hover {
+    color: ${lighten(0.2, '#1565c0')};
+  }
 `;
